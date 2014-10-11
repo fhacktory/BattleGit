3fd1c1fee916dd0a5255f393f34c2e1e2da6b352
Stephane Seng <stephane.seng@attestationlegale.fr>
2014-10-11 19:08:24 +0200

    The retreiveService now retreives the committed files.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,20 +1,39 @@
 'use strict';
 
 angular.module('battleGitApp')
-        .factory('retreiveService', function ($window, $http) {
+        .factory('retreiveService', function ($http) {
+          var clientQuery = '?client_id=3bb9d435e94403d10de1&client_secret=eeeb700e0f2e679851fece64b0b84fba8fa35afd';
+
           return {
-            retreiveCommits: function (repositoryID) {
-              var promise = $http.get('https://api.github.com/repos/' + repositoryID + '/commits')
+            retreiveCommit: function (repositoryId, commitSha) {
+              var promise = $http.get('https://api.github.com/repos/' + repositoryId + '/commits/' + commitSha + clientQuery)
+                      .success(function (data, status) {
+                        var commit;
+                        commit = {
+                          sha: data.sha,
+                          files: data.files
+                        };
+                        return commit;
+                      })
+                      .error(function () {
+                        alert('ERROR');
+                      });
+
+              return promise;
+            },
+            retreiveCommits: function (repositoryId) {
+              var promise = $http.get('https://api.github.com/repos/' + repositoryId + '/commits' + clientQuery)
                       .success(function (data) {
                         var commits = [];
                         data.forEach(function (element) {
-                          commits.push({
+                          var commit = {
                             sha: element.sha,
                             date: element.committer.date,
                             message: element.message,
                             committerLogin: element.committer.login,
                             committerId: element.committer.id
-                          });
+                          };
+                          commits.push(commit);
                         });
                         return commits;
                       })
@@ -24,15 +43,16 @@ angular.module('battleGitApp')
 
               return promise;
             },
-            retreiveContributors: function (repositoryID) {
-              var promise = $http.get('https://api.github.com/repos/' + repositoryID + '/contributors')
+            retreiveContributors: function (repositoryId) {
+              var promise = $http.get('https://api.github.com/repos/' + repositoryId + '/contributors' + clientQuery)
                       .success(function (data) {
                         var contributors = [];
                         data.forEach(function (element) {
-                          contributors.push({
+                          var contributor = {
                             login: element.login,
                             id: element.id
-                          });
+                          };
+                          contributors.push(contributor);
                         });
                         return contributors;
                       })
@@ -45,14 +65,21 @@ angular.module('battleGitApp')
           };
         })
         .controller('MainCtrl', ['$scope', '$http', 'retreiveService', function ($scope, $http, retreiveService) {
-            $scope.repositoryID = 'angular/angular.js';
+            $scope.repositoryId = 'angular/angular.js';
 
             $scope.retreive = function () {
-              $scope.commits = [];
-              retreiveService.retreiveCommits($scope.repositoryID).then(function (response) {
+              retreiveService.retreiveCommits($scope.repositoryId).then(function (response) {
                 $scope.commits = response.data;
+
+                $scope.commits.forEach(function (commit, index, array) {
+                  retreiveService.retreiveCommit($scope.repositoryId, commit.sha).then(function (response) {
+                    // Reference modification.
+                    array[index].files = response.data.files;
+                  });
+                });
               });
-              retreiveService.retreiveContributors($scope.repositoryID).then(function (response) {
+
+              retreiveService.retreiveContributors($scope.repositoryId).then(function (response) {
                 $scope.contributors = response.data;
               });
             };
