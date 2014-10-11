866531cd9d2c9a307a6dde49fc66c19c6662199d
Stephane Seng <stephane.seng@attestationlegale.fr>
2014-10-11 22:19:39 +0200

    Using the previous committer of the same file as a relationship between developers.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,12 +5,6 @@ angular.module('battleGitApp')
             retreiveCommit: function (repositoryId, commitSha, clientId, clientSecret) {
               var promise = $http.get('https://api.github.com/repos/' + repositoryId + '/commits/' + commitSha + '?client_id=' + clientId + '&client_secret=' + clientSecret)
                       .success(function (data, status) {
-                        var commit;
-                        commit = {
-                          sha: data.sha,
-                          files: data.files
-                        };
-                        return commit;
                       })
                       .error(function () {
                         alert('ERROR');
@@ -20,19 +14,6 @@ angular.module('battleGitApp')
             retreiveCommits: function (repositoryId, since, until, page, perPage, clientId, clientSecret) {
               var promise = $http.get('https://api.github.com/repos/' + repositoryId + '/commits' + '?since=' + since + '&until=' + until + '&page=' + page + '&per_page=' + perPage + '&client_id=' + clientId + '&client_secret=' + clientSecret)
                       .success(function (data) {
-                        var commits = [];
-                        data.forEach(function (element) {
-                          var commit = {
-                            sha: element.sha,
-                            date: element.committer.date,
-                            message: element.commit.message,
-                            committerLogin: element.committer.login,
-                            committerId: element.committer.id
-                          };
-
-                          commits.push(commit);
-                        });
-                        return commits;
                       })
                       .error(function () {
                         alert('ERROR');
@@ -42,62 +23,70 @@ angular.module('battleGitApp')
             retreiveContributors: function (repositoryId, clientId, clientSecret) {
               var promise = $http.get('https://api.github.com/repos/' + repositoryId + '/contributors' + '?client_id=' + clientId + '&client_secret=' + clientSecret)
                       .success(function (data) {
-                        var contributors = [];
-                        data.forEach(function (element) {
-                          var contributor = {
-                            login: element.login,
-                            id: element.id
-                          };
-                          contributors.push(contributor);
-                        });
-                        return contributors;
+                      })
+                      .error(function () {
+                        alert('ERROR');
+                      });
+              return promise;
+            },
+            retreivePreviousCommitter: function (repositoryId, filename, clientId, clientSecret) {
+              var promise = $http.get('https://api.github.com/repos/' + repositoryId + '/commits' + '?path=' + filename + '&page=1&per_page=1&client_id=' + clientId + '&client_secret=' + clientSecret)
+                      .success(function (data) {
                       })
                       .error(function () {
                         alert('ERROR');
                       });
               return promise;
             }
-//            ,
-//            retreiveFile: function (repositoryId, fileBlobUrl, clientId, clientSecret) {
-//              var promise = $http.get(fileBlameUrl)
-//                      .success(function (data) {
-//                        console.log(data);
-//                      })
-//                      .error(function (data, status) {
-//                        alert(fileBlameUrl);
-//                        alert('ERROR: ' + status);
-//                      });
-//              return promise;
-//            }
           };
         })
         .controller('MainCtrl', ['$scope', '$http', 'retreiveService', function ($scope, $http, retreiveService) {
             $scope.clientId = '3bb9d435e94403d10de1';
             $scope.clientSecret = 'eeeb700e0f2e679851fece64b0b84fba8fa35afd';
             $scope.repositoryId = 'angular/angular.js';
-            $scope.since = '2014-10-11' + 'T00:00:00Z';
+            $scope.since = '2014-01-01' + 'T00:00:00Z';
             $scope.until = '2014-10-11' + 'T24:00:00Z';
             $scope.page = '0';
             $scope.perPage = '1';
 
             $scope.retreive = function () {
-//              retreiveService.retreiveContributors($scope.repositoryId, $scope.clientId, $scope.clientSecret).then(function (response) {
-//                $scope.contributors = response.data;
-//              });
+              retreiveService.retreiveContributors($scope.repositoryId, $scope.clientId, $scope.clientSecret).then(function (response) {
+                $scope.contributors = [];
+                response.data.forEach(function (element) {
+                  var contributor = {
+                    login: element.login,
+                    id: element.id
+                  };
+                  $scope.contributors.push(contributor);
+                });
+              });
 
               retreiveService.retreiveCommits($scope.repositoryId, $scope.since, $scope.until, $scope.page, $scope.perPage, $scope.clientId, $scope.clientSecret).then(function (response) {
-                $scope.commits = response.data;
+                $scope.commits = [];
+                response.data.forEach(function (element) {
+                  var commit = {
+                    sha: element.sha,
+                    date: element.commit.committer.date,
+                    message: element.commit.message,
+                    committerLogin: element.committer.login,
+                    committerId: element.committer.id
+                  };
+                  $scope.commits.push(commit);
+                });
 
                 // Each retreived commit is processed one at a time.
-                $scope.commits.forEach(function (commit, index, array) {
+                $scope.commits.forEach(function (commit, commitIndex, commitArray) {
                   retreiveService.retreiveCommit($scope.repositoryId, commit.sha, $scope.clientId, $scope.clientSecret).then(function (response) {
                     // Reference modification.
-                    array[index].files = response.data.files;
+                    commitArray[commitIndex].files = response.data.files;
 
                     response.data.files.forEach(function (file, fileIndex, fileArray) {
-//                      retreiveService.retreiveFile($scope.repositoryId, file.blob_url, $scope.clientId, $scope.clientSecret).then(function (response) {
-//
-//                      });
+                      retreiveService.retreivePreviousCommitter($scope.repositoryId, file.filename, $scope.clientId, $scope.clientSecret).then(function (response) {
+                        fileArray[fileIndex].previousCommitter = {
+                          login: response.data[0].committer.login,
+                          id: response.data[0].committer.id
+                        };
+                      });
                     });
                   });
                 });
