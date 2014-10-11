2c1bd98520de6e26ae983e98fe8bcb6b6828c246
Stephane Seng <stephane.seng@attestationlegale.fr>
2014-10-11 20:06:01 +0200

    Added since and until parameters when retreiving GitHub data.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,12 +1,9 @@
 'use strict';
-
 angular.module('battleGitApp')
         .factory('retreiveService', function ($http) {
-          var clientQuery = '?client_id=3bb9d435e94403d10de1&client_secret=eeeb700e0f2e679851fece64b0b84fba8fa35afd';
-
           return {
-            retreiveCommit: function (repositoryId, commitSha) {
-              var promise = $http.get('https://api.github.com/repos/' + repositoryId + '/commits/' + commitSha + clientQuery)
+            retreiveCommit: function (repositoryId, commitSha, clientId, clientSecret) {
+              var promise = $http.get('https://api.github.com/repos/' + repositoryId + '/commits/' + commitSha + '?client_id=' + clientId + '&client_secret=' + clientSecret)
                       .success(function (data, status) {
                         var commit;
                         commit = {
@@ -18,21 +15,21 @@ angular.module('battleGitApp')
                       .error(function () {
                         alert('ERROR');
                       });
-
               return promise;
             },
-            retreiveCommits: function (repositoryId) {
-              var promise = $http.get('https://api.github.com/repos/' + repositoryId + '/commits' + clientQuery)
+            retreiveCommits: function (repositoryId, since, until, clientId, clientSecret) {
+              var promise = $http.get('https://api.github.com/repos/' + repositoryId + '/commits' + '?since=' + since + '&until=' + until + '&client_id=' + clientId + '&client_secret=' + clientSecret)
                       .success(function (data) {
                         var commits = [];
                         data.forEach(function (element) {
                           var commit = {
                             sha: element.sha,
                             date: element.committer.date,
-                            message: element.message,
+                            message: element.commit.message,
                             committerLogin: element.committer.login,
                             committerId: element.committer.id
                           };
+
                           commits.push(commit);
                         });
                         return commits;
@@ -40,11 +37,10 @@ angular.module('battleGitApp')
                       .error(function () {
                         alert('ERROR');
                       });
-
               return promise;
             },
-            retreiveContributors: function (repositoryId) {
-              var promise = $http.get('https://api.github.com/repos/' + repositoryId + '/contributors' + clientQuery)
+            retreiveContributors: function (repositoryId, clientId, clientSecret) {
+              var promise = $http.get('https://api.github.com/repos/' + repositoryId + '/contributors' + '?client_id=' + clientId + '&client_secret=' + clientSecret)
                       .success(function (data) {
                         var contributors = [];
                         data.forEach(function (element) {
@@ -59,28 +55,30 @@ angular.module('battleGitApp')
                       .error(function () {
                         alert('ERROR');
                       });
-
               return promise;
             }
           };
         })
         .controller('MainCtrl', ['$scope', '$http', 'retreiveService', function ($scope, $http, retreiveService) {
+            $scope.clientId = '3bb9d435e94403d10de1';
+            $scope.clientSecret = 'eeeb700e0f2e679851fece64b0b84fba8fa35afd';
             $scope.repositoryId = 'angular/angular.js';
+            $scope.since = '2014-10-11T00:00:00Z';
+            $scope.until = '2014-10-11T24:00:00Z';
 
             $scope.retreive = function () {
-              retreiveService.retreiveCommits($scope.repositoryId).then(function (response) {
+              retreiveService.retreiveCommits($scope.repositoryId, $scope.since, $scope.until, $scope.clientId, $scope.clientSecret).then(function (response) {
                 $scope.commits = response.data;
-
                 $scope.commits.forEach(function (commit, index, array) {
-                  retreiveService.retreiveCommit($scope.repositoryId, commit.sha).then(function (response) {
+                  retreiveService.retreiveCommit($scope.repositoryId, commit.sha, $scope.clientId, $scope.clientSecret).then(function (response) {
                     // Reference modification.
                     array[index].files = response.data.files;
                   });
                 });
               });
 
-              retreiveService.retreiveContributors($scope.repositoryId).then(function (response) {
-                $scope.contributors = response.data;
-              });
+//              retreiveService.retreiveContributors($scope.repositoryId, $scope.clientId, $scope.clientSecret).then(function (response) {
+//                $scope.contributors = response.data;
+//              });
             };
           }]);
