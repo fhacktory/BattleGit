f96d7f772201e41ad975ddcd68fa80ee2c6f3b4d
Stephane Seng <stephane.seng@attestationlegale.fr>
2014-10-11 23:17:33 +0200

    Added the main loop of the game.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -40,56 +40,68 @@ angular.module('battleGitApp')
             }
           };
         })
-        .controller('MainCtrl', ['$scope', '$http', 'retreiveService', function ($scope, $http, retreiveService) {
+        .controller('MainCtrl', ['$scope', '$interval', 'retreiveService', function ($scope, $interval, retreiveService) {
             $scope.clientId = '3bb9d435e94403d10de1';
             $scope.clientSecret = 'eeeb700e0f2e679851fece64b0b84fba8fa35afd';
             $scope.repositoryId = 'angular/angular.js';
             $scope.since = '2014-01-01' + 'T00:00:00Z';
             $scope.until = '2014-10-11' + 'T24:00:00Z';
-            $scope.page = '0';
-            $scope.perPage = '1';
+            $scope.interval = 2000;
+            $scope.page = 0;
+            $scope.perPage = 1;
+            $scope.maxSteps = 10;
 
-            $scope.retreive = function () {
-              retreiveService.retreiveContributors($scope.repositoryId, $scope.clientId, $scope.clientSecret).then(function (response) {
-                $scope.contributors = [];
-                response.data.forEach(function (element) {
-                  var contributor = {
-                    login: element.login,
-                    id: element.id
-                  };
-                  $scope.contributors.push(contributor);
-                });
+            // Global initialization.
+            $scope.users = {};
+
+            // Users initialization.
+            retreiveService.retreiveContributors($scope.repositoryId, $scope.clientId, $scope.clientSecret).then(function (response) {
+              response.data.forEach(function (element) {
+                $scope.users[element.id] = {
+                  login: element.login,
+                  id: element.id
+                };
               });
+            });
 
-              retreiveService.retreiveCommits($scope.repositoryId, $scope.since, $scope.until, $scope.page, $scope.perPage, $scope.clientId, $scope.clientSecret).then(function (response) {
-                $scope.commits = [];
-                response.data.forEach(function (element) {
-                  var commit = {
-                    sha: element.sha,
-                    date: element.commit.committer.date,
-                    message: element.commit.message,
-                    committerLogin: element.committer.login,
-                    committerId: element.committer.id
-                  };
-                  $scope.commits.push(commit);
-                });
+            // Main loop.
+            $scope.step = 0;
+            $scope.run = function () {
+              $interval(function () {
+                $scope.step++;
 
-                // Each retreived commit is processed one at a time.
-                $scope.commits.forEach(function (commit, commitIndex, commitArray) {
-                  retreiveService.retreiveCommit($scope.repositoryId, commit.sha, $scope.clientId, $scope.clientSecret).then(function (response) {
-                    // Reference modification.
-                    commitArray[commitIndex].files = response.data.files;
+                retreiveService.retreiveCommits($scope.repositoryId, $scope.since, $scope.until, $scope.page + $scope.step, $scope.perPage, $scope.clientId, $scope.clientSecret).then(function (response) {
+                  $scope.commits = [];
+                  response.data.forEach(function (element) {
+                    var commit = {
+                      sha: element.sha,
+                      date: element.commit.committer.date,
+                      message: element.commit.message,
+                      committerLogin: element.committer.login,
+                      committerId: element.committer.id
+                    };
+                    $scope.commits.push(commit);
+                  });
 
-                    response.data.files.forEach(function (file, fileIndex, fileArray) {
-                      retreiveService.retreivePreviousCommitter($scope.repositoryId, file.filename, $scope.clientId, $scope.clientSecret).then(function (response) {
-                        fileArray[fileIndex].previousCommitter = {
-                          login: response.data[0].committer.login,
-                          id: response.data[0].committer.id
-                        };
+                  // Each retreived commit is processed one at a time.
+                  $scope.commits.forEach(function (commit, commitIndex, commitArray) {
+                    retreiveService.retreiveCommit($scope.repositoryId, commit.sha, $scope.clientId, $scope.clientSecret).then(function (response) {
+                      // Reference modification.
+                      commitArray[commitIndex].files = response.data.files;
+
+                      response.data.files.forEach(function (file, fileIndex, fileArray) {
+                        retreiveService.retreivePreviousCommitter($scope.repositoryId, file.filename, $scope.clientId, $scope.clientSecret).then(function (response) {
+                          fileArray[fileIndex].previousCommitter = {
+                            login: response.data[0].committer.login,
+                            id: response.data[0].committer.id
+                          };
+
+                          // All the data is ready.
+                        });
                       });
                     });
                   });
                 });
-              });
+              }, $scope.interval, $scope.maxSteps);
             };
           }]);
