06d9f27174c0505f722ae4089268a15516383552
Stephane Seng <stephane.seng@attestationlegale.fr>
2014-10-11 18:21:09 +0200

    The retreiveService retreives commits and committers.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,21 +1,59 @@
 'use strict';
 
 angular.module('battleGitApp')
-        .factory('retreiveService', function ($window) {
-          return function (repositoryURL) {
-            $window.alert(repositoryURL);
+        .factory('retreiveService', function ($window, $http) {
+          return {
+            retreiveCommits: function (repositoryID) {
+              var promise = $http.get('https://api.github.com/repos/' + repositoryID + '/commits')
+                      .success(function (data) {
+                        var commits = [];
+                        data.forEach(function (element) {
+                          commits.push({
+                            sha: element.sha,
+                            date: element.committer.date,
+                            message: element.message,
+                            committerLogin: element.committer.login,
+                            committerId: element.committer.id
+                          });
+                        });
+                        return commits;
+                      })
+                      .error(function () {
+                        alert('ERROR');
+                      });
+
+              return promise;
+            },
+            retreiveContributors: function (repositoryID) {
+              var promise = $http.get('https://api.github.com/repos/' + repositoryID + '/contributors')
+                      .success(function (data) {
+                        var contributors = [];
+                        data.forEach(function (element) {
+                          contributors.push({
+                            login: element.login,
+                            id: element.id
+                          });
+                        });
+                        return contributors;
+                      })
+                      .error(function () {
+                        alert('ERROR');
+                      });
+
+              return promise;
+            }
           };
         })
-        .controller('MainCtrl', function ($scope, retreiveService) {
-          $scope.repositoryURL = 'https://github.com/fhacktory/BattleGit';
+        .controller('MainCtrl', ['$scope', '$http', 'retreiveService', function ($scope, $http, retreiveService) {
+            $scope.repositoryID = 'angular/angular.js';
 
-          $scope.awesomeThings = [
-            'HTML5 Boilerplate',
-            'AngularJS',
-            'Karma'
-          ];
-
-          $scope.retreive = function () {
-            retreiveService($scope.repositoryURL);
-          };
-        });
+            $scope.retreive = function () {
+              $scope.commits = [];
+              retreiveService.retreiveCommits($scope.repositoryID).then(function (response) {
+                $scope.commits = response.data;
+              });
+              retreiveService.retreiveContributors($scope.repositoryID).then(function (response) {
+                $scope.contributors = response.data;
+              });
+            };
+          }]);
